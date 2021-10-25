1a4f6d205c864746afa38d1ce444dd75ffe20bf4
vinceeeent <vincent.ontuca@evsu.edu.ph>
2021-10-25 15:15:21 +0800

    added sideNav and Toolbar as default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,17 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
     ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    RouterModule.forRoot([
+      { path: 'home', component: ProfileComponent },
+      { path: 'contacts', component: ContactsComponent },
+      { path: 'gallery', component: GalleryComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
